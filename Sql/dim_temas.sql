20804769bbf5a8c1efcbdfaa73e925730d4969de
juancarloscm <juancarloscm@yahoo.com>
2025-01-31 22:47:59 -0500

    Update dim_temas.sql

--- Sql/dim_temas.sql
+++ Sql/dim_temas.sql
@@ -1,1 +1,6 @@
+CREATE OR REPLACE TABLE `analitica-contact-center-dev.Entorno_Pruebas_modelo.dim_temas` (
+    id_tema INT64,       -- Identificador unico del tema
+    nombre_tema STRING,  -- Nombre del tema
+    categoria STRING     -- Categoria general del tema
+);
 
