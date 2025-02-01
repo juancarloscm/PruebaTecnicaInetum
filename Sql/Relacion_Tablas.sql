6794db3acf59508322742e1abc76949fb39831d9
juancarloscm <juancarloscm@yahoo.com>
2025-01-31 22:50:52 -0500

    Update Relacion_Tablas.sql

--- Sql/Relacion_Tablas.sql
+++ Sql/Relacion_Tablas.sql
@@ -1,1 +1,15 @@
+-- Relaciones Entre las Tablas
+SELECT 
+    f.nombre_fuente, 
+    t.nombre_tema, 
+    n.fecha_publicacion, 
+    n.titulo, 
+    n.visitas, 
+    n.compartidos
+FROM `analitica-contact-center-dev.Entorno_Pruebas_modelo.noticias_procesadas` n
+JOIN `analitica-contact-center-dev.Entorno_Pruebas_modelo.dim_fuentes_noticias` f 
+    ON n.id_fuente = f.id_fuente
+JOIN `analitica-contact-center-dev.Entorno_Pruebas_modelo.dim_temas` t 
+    ON n.id_tema = t.id_tema
+ORDER BY n.fecha_publicacion DESC;
 
