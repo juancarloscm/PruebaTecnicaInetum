65ea7c7a14047c54499446ae9d327d4aa9a74aa6
juancarloscm <juancarloscm@yahoo.com>
2025-02-01 00:01:52 -0500

    Update Tendencias_Temas_Mes.sql

--- Data_Warehouse_Bigquery/Tendencias_Temas_Mes.sql
+++ Data_Warehouse_Bigquery/Tendencias_Temas_Mes.sql
@@ -1,1 +1,10 @@
+SELECT 
+    FORMAT_DATE('%Y-%m', fecha_publicacion) AS mes,
+    t.nombre AS tema,
+    COUNT(*) AS total_articulos
+FROM `analitica-contact-center-dev.Entorno_Pruebas_modelo.fact_articulos` f
+JOIN `analitica-contact-center-dev.Entorno_Pruebas_modelo.dim_temas` t 
+    ON f.topic_id = t.topic_id
+GROUP BY mes, tema
+ORDER BY mes DESC, total_articulos DESC;
 
