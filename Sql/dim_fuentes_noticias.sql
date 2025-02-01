a09873be065f7438659c098c057407417e4998ad
juancarloscm <juancarloscm@yahoo.com>
2025-01-31 22:46:55 -0500

    Update dim_fuentes_noticias.sql

--- Sql/dim_fuentes_noticias.sql
+++ Sql/dim_fuentes_noticias.sql
@@ -1,1 +1,7 @@
+CREATE OR REPLACE TABLE `analitica-contact-center-dev.Entorno_Pruebas_modelo.dim_fuentes_noticias` (
+    id_fuente INT64,         -- Identificador unico de la fuente
+    nombre_fuente STRING,    -- Nombre del sitio de noticias
+    url STRING,              -- Enlace al sitio de noticias
+    puntuacion_confiabilidad FLOAT64 -- Puntaje de confiabilidad de la fuente
+);
 
